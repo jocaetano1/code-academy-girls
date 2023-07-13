1559533907f20b9b57605ddd60791d15934bfb36
josan5368 <josan5368@gmail.com>
2023-07-14 00:31:17 +0100

    feat: visualizar lista de presenças

--- scripts/ponto_de_acesso_electronico.py
+++ scripts/ponto_de_acesso_electronico.py
@@ -21,6 +21,7 @@ Menu de opções:
 [1] - Inserir nova aluna
 [2] - Registrar presença
 [3] - Visualizar alunas
+[4] - Visualizar presenças
 [9] - Sair
 
 """)
@@ -41,9 +42,9 @@ Menu de opções:
         aluna = alunas.get(numero_do_bi)
         if aluna is None:
             alunas[numero_do_bi] = formanda
-            print("Aluna registrada com sucesso.")
+            print("\n Aluna registrada com sucesso.")
         else:
-            print("Aluna já foi registrada.")
+            print("\n Aluna já foi registrada.")
 
     if opcao == "2":
         identidade = input("Digite o número de seu B.I: ")
@@ -54,44 +55,58 @@ Menu de opções:
                 hora_da_entrada = input("Digite a hora de entrada: ")
                 presenca = {
                     identidade: {
-                        "aluna": aluna,
+                        **aluna,
                         "hora_de_entrada": hora_da_entrada,
                         "hora_de_saida": "",
                         "data": date.today().isoformat()
                     }
                 }
                 presencas.append(presenca)
-            elif len(presencas) >= 1:
+            else:
                 for presenca in presencas:
-                    registro_de_presenca = presenca.get(identidade)
-                    if registro_de_presenca is None:
-                        hora_da_entrada = input("Digite a hora de entrada: ")
-                        presenca = {
-                            identidade: {
-                                "aluna": aluna,
-                                "hora_de_entrada": hora_da_entrada,
-                                "hora_de_saida": "",
-                                "data": date.today().isoformat()
-                            }
-                        }
-                        presencas.append(presenca)
+                    if identidade in presenca.keys():
+                        registro_de_presenca = presenca.get(identidade)
+                        break
                     else:
-                        hora_de_saida = registro_de_presenca.get(
-                            "hora_de_saida")
-                        data = registro_de_presenca.get("data")
-                        hoje = date.today().isoformat()
-                        if data == hoje and hora_de_saida == "":
-                            hora_de_saida = input("Digite a hora de saída: ")
-                            registro_de_presenca["hora_de_saida"] = hora_de_saida
-                        else:
-                            print("A presença já foi registrada.")
+                        registro_de_presenca = None
+                if registro_de_presenca is None:
+                    hora_da_entrada = input("Digite a hora de entrada: ")
+                    presenca = {
+                        identidade: {
+                            **aluna,
+                            "hora_de_entrada": hora_da_entrada,
+                            "hora_de_saida": "",
+                            "data": date.today().isoformat()
+                        }
+                    }
+                    presencas.append(presenca)
+                else:
+                    hora_de_saida = registro_de_presenca.get(
+                        "hora_de_saida")
+                    data = registro_de_presenca.get("data")
+                    hoje = date.today().isoformat()
+                    if data == hoje and hora_de_saida == "":
+                        hora_de_saida = input("Digite a hora de saída: ")
+                        registro_de_presenca["hora_de_saida"] = hora_de_saida
+                    else:
+                        print("A presença já foi registrada.")
         else:
-            print("O número do B.I não corresponde aos dados de nenhuma aluna.")
+            print("\n O número do B.I não corresponde aos dados de nenhuma aluna.")
 
     if opcao == "3":
         for index, aluna in enumerate(alunas.values()):
             mensagem = f"{index + 1}º - {aluna.get('nome')}"
             print(mensagem)
+    if opcao == "4":
+        print("\n ******************* Lista de presenças ******************* \n")
+        for index, presenca in enumerate(presencas):
+            for _, valor in presenca.items():
+                nome = valor.get("nome")
+                hora_de_entrada = valor.get("hora_de_entrada")
+                hora_de_saida = valor.get("hora_de_saida")
+                data = valor.get("data")
+                print(
+                    f"{index + 1}º - {data}: {nome} == ({hora_de_entrada}, {hora_de_saida})")
 
     if opcao == "9":
         print("""
